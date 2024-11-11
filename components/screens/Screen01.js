dcab2f101e994585d7aca34737d502dafdf976a5
Tran Hien Vinh <tranvinh141203@gmail.com>
2024-11-11 16:03:41 +0700

    Cập nhật giao diện màn hình Screen01

--- components/screens/Screen01.js
+++ components/screens/Screen01.js
@@ -1,67 +1,89 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
 export default function Screen01() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>
-        A premium online store for sporter and their stylish choice
-      </Text>
-      <View style={styles.imageContainer}>
-        <Image
-          source={{ uri: 'https://i.ibb.co/4Vs6qvg/bifour-removebg-preview-1.png' }}
-          style={styles.image}
-          resizeMode="contain"
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>
+          A premium online store for {'\n'} sporter and their stylish choice
+        </Text>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{
+              uri: "https://i.ibb.co/4Vs6qvg/bifour-removebg-preview-1.png",
+            }}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        </View>
+        <Text style={styles.title}>POWER BIKE SHOP</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonADD}>
+          <Text style={styles.buttonText}>ADD</Text>
+        </TouchableOpacity>
       </View>
-      <Text style={styles.title}>POWER BIKE SHOP</Text>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Get Started</Text>
-      </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5F5F5",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   headerText: {
     fontSize: 16,
-    textAlign: 'center',
-    color: '#000',
+    textAlign: "center",
+    color: "#000",
     marginBottom: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   imageContainer: {
-    backgroundColor: '#FFE6E6',
+    backgroundColor: "#FFE6E6",
     borderRadius: 20,
     padding: 20,
     marginBottom: 20,
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 300,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#000',
+    fontWeight: "bold",
+    color: "#000",
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#FF4D4D',
+    marginTop: 20,
+    backgroundColor: "#FF4D4D",
     borderRadius: 10,
     paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 150,
   },
   buttonText: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  buttonADD: {
+    marginTop: 20,
+    backgroundColor: "blue",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 175,
   },
 });
